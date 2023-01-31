c1477a31006fdbaf58451c9287421e39b1ed15d7
Luke <116241757+Nyrexis@users.noreply.github.com>
2023-01-31 17:41:43 +1100

    divided up functions (doesnt work)

--- index2.js
+++ index2.js
@@ -6,22 +6,33 @@ const inquirer = require('inquirer');
 const fs = require("fs");
 var employees = [];
 
-function init() {
-    if (employees = []){
+async function init() {
     console.log("Please input the details of the team manager")
-    }
+    let person = new Promise(baseEmployee())
+    await person
+    .then(
+    managerinfo(person))
     inquirer.prompt([
-        // {
-        // type: 'list',
-        // message: 'What type of employee would you like to add?',
-        // choices: [
-        //   "Engineer",
-        //   "Manager",
-        //   "Intern",
-        //   "Employee",
-        // ],
-        //   name: 'type',
-        // },
+        {
+          type: 'confirm',
+          message: 'Add another team member?',
+          name: 'continue',
+        },
+    ]).then((confirm) => {
+        switch (confirm){
+            case true: {
+                teamInfo(baseEmployee)
+                break
+                }
+            case false: {
+                //dont know yet
+                break
+            } 
+        }
+    })
+}
+function baseEmployee () {
+    inquirer.prompt([
         {
             type: 'input',
             message: 'Employee\'s name',
@@ -37,62 +48,62 @@ function init() {
               message: 'Employee\'s email address',
               name: 'email',
            },
-    ])
-    .then((data) => { 
-        if (employees = []){
-            inquirer.prompt([
-                {
-                  type: 'input',
-                  message: 'Manager\'s office number',
-                  name: 'officeNumber',
-                },
-            ]).then((managerData) => {
-                const manager = new Manager(data.name, data.id, data.email, managerData.officeNumber)
-                employees.push(manager)      
-            });
-        } else inquirer.prompt([
-            {
-              type: 'confirm',
-              message: 'Add another team member?',
-              name: 'continue',
-            },
-        ]).then((confirm) => {
-            switch (confirm){
-                case True: {
-                    inquirer.prompt([
-                        {
-                            type: 'input',
-                            message: 'Engineer\'s github profile',
-                            name: 'github',
-                        }]).then((engineerData) => {
-                            const engineer = new Engineer(data.name, data.id, data.email, engineerData.github)
-                            console.log(engineer)
-                            console.log(engineer.getRole()) 
-                            employees.push(engineer)     
-                        });
-                    break;
-                }
-                case "Manager": {
-                    
-                    break;
-                }
-                case "Intern": {
-                    inquirer.prompt([
-                        {
-                          type: 'input',
-                          message: 'School intern attends',
-                          name: 'school',
-                        },
-                    ]).then((internData) => {
-                        const intern = new Intern(data.name, data.id, data.email, internData.school)
-                        employees.push(intern)      
-                    });
-                    break;
-                }
-            }
-        } );      
-        });
-        
+    ]).then((data) => {
+        const employee = new Employee(data.name, data.id, data.email)
+        return employee      
+});
 }
-
+function managerinfo (employee){
+    inquirer.prompt([
+           {
+            type: 'input',
+            message: 'Manager\'s office number',
+            name: 'officeNumber',
+          },
+        ]).then((data) => {
+                const manager = new Manager(employee.name, employee.id, employee.email, data.officeNumber)
+                employees.push(manager)      
+        });
+    }
+    
+function teamInfo(employee){
+        inquirer.prompt([
+            {
+            type: 'list',
+            message: 'What type of employee would you like to add?',
+            choices: [
+                "Engineer",
+                "Intern",]
+            },
+        ]).then((data) => { 
+                switch (data.type){
+                    case "Engineer": {
+                        inquirer.prompt([
+                            {
+                                type: 'input',
+                                message: 'Engineer\'s github profile',
+                                name: 'github',
+                            },
+                        ]).then((engineerData) => {
+                                const engineer = new Engineer(employee.name, employee.id, employee.email, engineerData.github)
+                                employees.push(engineer)     
+                            });
+                        break;
+                    }
+                    case "Intern": {
+                        inquirer.prompt([
+                            {
+                                type: 'input',
+                                message: 'School intern attends',
+                                name: 'school',
+                            },
+                        ]).then((internData) => {
+                                const intern = new Intern(employee.name, employee.id, employee.email, internData.school)
+                                employees.push(intern)      
+                        });
+                        break;
+                    }
+                }
+            } );
+}
 init()
